e407ca0268497a76cc8afaa070cceddc6ede09ed
Alapan Bhakat <alapanbhakat@gmail.com>
2019-06-18 14:16:18 -0700

    updated friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,21 @@ module.exports = {
 			name: 'Alapan Bhakat',
 			photo: '/images/mypic.jpg',
 			scores: ['1','1','5','1','1','1','1','1','1','1']
+		},
+		{
+			name: 'Clark Kent',
+			photo: 'https://s3.amazonaws.com/comicgeeks/characters/avatars/19.jpg?t=1533123935',
+			scores: ['4','3','5','1','2','1','5','1','1','1']
+		},
+		{
+			name: 'Bruce Wayne',
+			photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/Bruce_Wayne_%28David_Mazouz%29.png/220px-Bruce_Wayne_%28David_Mazouz%29.png',
+			scores: ['5','5','1','1','1','1','1','1','1','1']
+		},
+		{
+			name: 'Diana Prince',
+			photo: 'https://static.tvtropes.org/pmwiki/pub/images/galgadot.jpg',
+			scores: ['3','3','1','2','5','1','2','1','1','1']
 		}
 	]
 }
